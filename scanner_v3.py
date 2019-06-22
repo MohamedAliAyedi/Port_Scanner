a58c06eb19dc61aa17d905f7e3a5c7f1a9239f49
Mohamed Ali Ayedi <46815881+MohamedAliAyedi@users.noreply.github.com>
2019-06-21 23:07:05 -0400

    Add files via upload

---
+++ scanner_v3.py
@@ -0,0 +1,18 @@
+from socket import *
+print("\n\n\t*******Port Scanner By Mohamed Ali Ayedi v3 *******")
+
+i=0 # compteur to compte nombre of port open
+ip=raw_input("TARGET IP >> ") # varibale str (ip)
+for port in range(0,10000): # loop for 0 > 10000 (port)
+ s=socket(AF_INET,SOCK_STREAM) # varibale to libary socket 
+ s.settimeout(5) # time out
+ if(s.connect_ex((ip,port))==0): # if reverse connection not null the port open ;)
+   print "\t\tPort",port, "  is Open  Service ("+getservbyport(port)+") is work !"
+ # print
+   i+=1 # compteur +1 if port open 
+ s.close() #close connection
+print(" *******************************************************************")
+print " * Scannig Host | "+ip+" | completed Found  > "+str(i)+" <  Port is Open *"
+print(" *******************************************************************\n\n")
+
+
